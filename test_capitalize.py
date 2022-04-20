61280521c08a0ba9fc9354200faafc45b4ac0538
Ye Tao (Janson) <40192895+Jansonboss@users.noreply.github.com>
2022-04-19 21:32:42 -0700

    First commit

---
+++ test_capitalize.py
@@ -0,0 +1,16 @@
+
+def capitalize_string(s):
+    if not isinstance(s, str):
+        raise TypeError('Please provide a string')
+    return s.capitalize()
+
+def lower_string(s):
+    if not isinstance(s, str):
+        raise TypeError('Please provide a string')
+    return s.lower()
+
+def test_capitalize_string():
+    assert capitalize_string('test') == 'Test'
+
+def test_lowercase_string():
+	assert lower_string('test') == 'test'
